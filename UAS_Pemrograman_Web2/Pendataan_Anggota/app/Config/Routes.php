aa0c19c4ecf785cd09b206ccc384cd579aeeffb8
kiki <149521424+pimngin@users.noreply.github.com>
2024-12-26 08:30:25 -0500

    Update Routes.php

--- UAS_Pemrograman_Web2/Pendataan_Anggota/app/Config/Routes.php
+++ UAS_Pemrograman_Web2/Pendataan_Anggota/app/Config/Routes.php
@@ -8,14 +8,14 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 // Rute user
-$routes->group('user',['filter' => 'group:user'], function($routes){
+//$routes->group('user',['filter' => 'group:user'], function($routes){
     $routes->get('anggota', 'Home::anggota');
     $routes->get('anggota', 'Home::anggota');
     $routes->post('anggota', "Home::create_anggota");
-});
+//});
 // Rute admin
 
-$routes->group('admin', ['filter' => 'group:admin'], function($routes) {    
+//$routes->group('admin', ['filter' => 'group:admin'], function($routes) {    
     $routes->get('admin/anggota', 'AdminController::anggota');
     $routes->post('admin/anggota', "AdminController::create_anggota");
     $routes->get('admin/anggota/(:segment)/edit', 'AdminController::edit_anggota/$1');
@@ -23,6 +23,8 @@ $routes->group('admin', ['filter' => 'group:admin'], function($routes) {
     $routes->get('/admin/anggota/delete/(:num)', 'AdminController::delete_anggota/$1');
     $routes->post('/hapus', 'AdminController::delete_anggota');
 
-});
+//});
+
+//Belajar menghubungkan halaman
 
 service('auth')->routes($routes);
